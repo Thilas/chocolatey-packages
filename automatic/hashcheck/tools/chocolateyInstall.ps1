b974b01ea0a5420c7b722d45b64ebdaf3d4e4f0c
Thilas <tdemoulins@gmail.com>
2015-07-14 18:01:49 +0200

    Add package for LAV Filters

--- automatic/hashcheck/tools/chocolateyInstall.ps1
+++ automatic/hashcheck/tools/chocolateyInstall.ps1
@@ -5,4 +5,4 @@ $url64 = $url
 $silentArgs = '/S'
 $validExitCodes = @(0)
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes $validExitCodes
--- automatic/lavfilters/tools/chocolateyInstall.ps1
+++ automatic/lavfilters/tools/chocolateyInstall.ps1
@@ -2,13 +2,7 @@
 $installerType = 'exe'
 $url = '{{DownloadUrl}}'
 $url64 = $url
-$silentArgs = '-y'
+$silentArgs = '/SILENT'
 $validExitCodes = @(0)
-
-$installOverride = $env:chocolateyInstallOverride
-if (!$installOverride) {
-  $env:IMDISK_SILENT_SETUP = 1
-  Write-Host "Silent mode set"
-}
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes $validExitCodes
