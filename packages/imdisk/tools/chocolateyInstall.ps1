6e6388e8469b35399b3842e1aaa13fe358ae1ae9
Thomas Démoulins <tdemoulins@enablon.net>
2015-12-14 14:28:24 +0100

    Remove old imdisk package

--- packages/imdisk/tools/chocolateyInstall.ps1
+++
@@ -1,8 +0,0 @@
-﻿$overrideArguments = $env:chocolateyInstallOverride
-if (!$overrideArguments)
-{
-    $env:IMDISK_SILENT_SETUP = 1
-    Write-Host "Silent mode set"
-}
-$url = 'http://www.ltr-data.se/files/imdiskinst.exe'
-Install-ChocolateyPackage 'imdisk' 'exe' '-y' "$url" "$url"
