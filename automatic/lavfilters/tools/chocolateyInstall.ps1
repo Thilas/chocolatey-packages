471d384142e9059e9226306f1562b00f930ede59
Thomas Démoulins <tdemoulins@enablon.net>
2016-12-11 19:36:20 +0100

    Update LAVFilter to version 0.69

--- automatic/lavfilters/tools/chocolateyInstall.ps1
+++ automatic/lavfilters/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$packageArgs = @{
   packageName    = 'lavfilters'
   fileType       = 'exe'
-  url            = 'https://github.com/Nevcairiel/LAVFilters/releases/download/0.68.1/LAVFilters-0.68.1-Installer.exe'
+  url            = 'https://github.com/Nevcairiel/LAVFilters/releases/download/0.69/LAVFilters-0.69-Installer.exe'
   silentArgs     = '/SILENT'
-  checksum       = '99fca55ad16c4388ee6222df475f44e7f3072f504e5cd6ff5c2c576a2dab8a3d'
+  checksum       = 'ca247ddea684e5000647b20cfeb0e91eb83b91faed14e15add07750f515369a6'
   checksumType   = 'sha256'
   validExitCodes = @(0)
 }
