17a52f4e9a1e25808a15758eb953be6480bd010f
Chocolatey <chocolatey@realdimensions.net>
2016-12-20 23:26:26 +0000

    AU: 1 updated - airserver [skip ci] https://gist.github.com/c09573f2f23bc2aa904d90ff63de2cfe/04fd6606331b66dadd8e301cf4f1449746d1b0ae

--- airserver/tools/chocolateyInstall.ps1
+++ airserver/tools/chocolateyInstall.ps1
@@ -1,14 +1,14 @@
 ﻿# *** Automatically filled ***
 $packageArgs = @{
-  packageName    = ''
-  fileType       = ''
-  url            = ''
-  url64bit       = ''
-  silentArgs     = ''
-  checksum       = ''
-  checksumType   = ''
-  checksum64     = ''
-  checksumType64 = ''
+  packageName    = 'airserver'
+  fileType       = 'msi'
+  url            = 'http://dl.airserver.com/pc32/AirServer-5.1.2-x86.msi'
+  url64bit       = 'http://dl.airserver.com/pc32/AirServer-5.1.2-x64.msi'
+  silentArgs     = '/quiet'
+  checksum       = '87eda7f1cb178eb2ac08c91a60d0b4c84be6fa5c3194172c3ac00a5f8bf42497'
+  checksumType   = 'sha256'
+  checksum64     = 'dd2fc9d4dd674456107f8bb7d30bea9a34e1fc830f768de24bb7556a14b2ad5b'
+  checksumType64 = 'sha256'
   validExitCodes = @(0)
 }
 # *** Automatically filled ***
