421a8c55ca35c99f8fd502aef7c243345b0c6309
Chocolatey <chocolatey@realdimensions.net>
2017-06-21 18:07:58 +0000

    AU: 1 updated - stellarium [skip ci] https://gist.github.com/c09573f2f23bc2aa904d90ff63de2cfe/feda0144fb54f41bcd0c37754feaccbf61d7fabe

--- stellarium/tools/chocolateyInstall.ps1
+++ stellarium/tools/chocolateyInstall.ps1
@@ -4,12 +4,12 @@
 $packageArgs = @{
   packageName    = 'stellarium'
   fileType       = 'exe'
-  url            = 'https://sourceforge.net/projects/stellarium/files/Stellarium-win32/0.15.2/stellarium-0.15.2-win32.exe/download'
-  url64bit       = 'https://sourceforge.net/projects/stellarium/files/Stellarium-win64/0.15.2/stellarium-0.15.2-win64.exe/download'
+  url            = 'https://sourceforge.net/projects/stellarium/files/Stellarium-win32/0.16.0/stellarium-0.16.0-win32.exe/download'
+  url64bit       = 'https://sourceforge.net/projects/stellarium/files/Stellarium-win64/0.16.0/stellarium-0.16.0-win64.exe/download'
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-  checksum       = '2f8a4df1d80a862e6d7822c65bbf2556cc7dc9bd811108e3abab1e1cf609e121'
+  checksum       = '373fa59806fd06d82da1bbca373fa5a4df869f33eb077cba39afaf7e29152b16'
   checksumType   = 'sha256'
-  checksum64     = '785b7f420872d177f40b70b565e757fc26a66871f3c75c957452ae2b1b6638a0'
+  checksum64     = 'bdf6017b6767552c12fef3b3f81a20d593254bdc2ffe41e075ac5647c32015e3'
   checksumType64 = 'sha256'
   validExitCodes = @(0)
 }
