363849e1ef6545e6c466b969e9a9cc2149eb3cf8
Chocolatey <chocolatey@realdimensions.net>
2017-09-28 23:12:37 +0000

    AU: 1 updated - avgantivirusfree [skip ci] https://gist.github.com/c09573f2f23bc2aa904d90ff63de2cfe/414cab1d2a668defe4f8b69b4e68808835a54ec2

--- avgantivirusfree/tools/chocolateyInstall.ps1
+++ avgantivirusfree/tools/chocolateyInstall.ps1
@@ -7,9 +7,9 @@ $packageArgs = @{
   url            = 'https://files-download.avg.com/inst/mp/Antivirus_Free_x86_1819.exe'
   url64bit       = 'https://files-download.avg.com/inst/mp/Antivirus_Free_x64_1818.exe'
   silentArgs     = "/mode=offline /install=fmw,zen,bav /langid=$((Get-UICulture).LCID) /silent=true"
-  checksum       = '17ea4e1ca5658e8adf3a526f38a20d2a9c4f4c1911bf6efff8db9c14c769bc03'
+  checksum       = 'ab910549ed460b7b9c19f3a57492eb8685b93da40d7f5cb21f395a1b5c28839c'
   checksumType   = 'sha256'
-  checksum64     = '197e26b2863125707bb38e765f0e5910e2941f3e07ba59fe59a491c7a19d7b0c'
+  checksum64     = '38a12cbdc4b8494be0c64052e45727fb19535416af217d8b2798274e916e52d3'
   checksumType64 = 'sha256'
   validExitCodes = @(0)
 }
