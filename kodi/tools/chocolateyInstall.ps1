ea6c89a48a105ec68d43bd026f4c600b659fe9f8
Chocolatey <chocolatey@realdimensions.net>
2017-10-24 00:04:31 +0000

    AU: 1 updated - kodi [skip ci] https://gist.github.com/c09573f2f23bc2aa904d90ff63de2cfe/4569648a898f85a6fcece26413bf62e27e13e519

--- kodi/tools/chocolateyInstall.ps1
+++ kodi/tools/chocolateyInstall.ps1
@@ -4,12 +4,12 @@
 $packageArgs = @{
   packageName    = 'kodi'
   fileType       = 'exe'
-  url            = 'http://mirrors.kodi.tv/releases/windows/win32/kodi-17.4-Krypton-x86.exe'
-  url64bit       = 'http://mirrors.kodi.tv/releases/windows/win32/kodi-17.4-Krypton-x86.exe'
+  url            = 'http://mirrors.kodi.tv/releases/windows/win32/kodi-17.5-Krypton-x86.exe'
+  url64bit       = 'http://mirrors.kodi.tv/releases/windows/win32/kodi-17.5-Krypton-x86.exe'
   silentArgs     = '/S'
-  checksum       = '940be9469a75a7f00f7e443a6de7c3d20dc5d485085f8117dc376b87dae52459'
+  checksum       = '3bed2f5e0f7dce622a4775ed848e86a98b48e45f7c7b1bd7635bafe559ff1a1b'
   checksumType   = 'sha256'
-  checksum64     = '940be9469a75a7f00f7e443a6de7c3d20dc5d485085f8117dc376b87dae52459'
+  checksum64     = '3bed2f5e0f7dce622a4775ed848e86a98b48e45f7c7b1bd7635bafe559ff1a1b'
   checksumType64 = 'sha256'
   validExitCodes = @(0)
 }
