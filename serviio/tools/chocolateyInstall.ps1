5b0d1a98840d66675ea28c180d74a9852e9c071b
Chocolatey <chocolatey@realdimensions.net>
2018-02-16 00:10:57 +0000

    AU: 1 updated - serviio [skip ci] https://gist.github.com/c09573f2f23bc2aa904d90ff63de2cfe/9f271a1701bc13a84b3eddc5f1eb052d44f5f844

--- serviio/tools/chocolateyInstall.ps1
+++ serviio/tools/chocolateyInstall.ps1
@@ -4,9 +4,9 @@
 $packageArgs = @{
   packageName    = 'serviio'
   fileType       = 'exe'
-  url            = 'http://download.serviio.org/releases/serviio-1.9-win-setup.exe'
+  url            = 'http://download.serviio.org/releases/serviio-1.9.1-win-setup.exe'
   silentArgs     = '/S'
-  checksum       = 'd4e4715ed12895f22fbc9c34de676b23c3ac0927e763b43fb8cec97f894bf687'
+  checksum       = '8b5b8e2e86a8e9dc0691b8e1fed98cfc1cc3f03802a67bfbc6bc54dbceac352f'
   checksumType   = 'sha256'
   validExitCodes = @(0)
 }
