d18e9771d7aefba0748a5e7205a626a5a7748c0e
Thomas <thomas.demoulins@enablon.com>
2018-03-15 08:29:00 +0100

    Update ilspy to v3.0.1

--- ilspy/tools/chocolateyInstall.ps1
+++ ilspy/tools/chocolateyInstall.ps1
@@ -4,11 +4,11 @@ $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 # *** Automatically filled ***
 $packageArgs = @{
-  packageName    = ''
-  url            = ''
+  packageName    = 'ilspy'
+  url            = 'https://github.com/icsharpcode/ILSpy/releases/download/v3.0.1/ILSpy_binaries_Net46_Win_3.0.1.3459.zip'
   unzipLocation  = $toolsDir
-  checksum       = ''
-  checksumType   = ''
+  checksum       = '5aca34b25fa416cc76af3a98d663791d951fdd6455f2d8b4e9e4520a315e4346'
+  checksumType   = 'sha256'
 }
 # *** Automatically filled ***
 
