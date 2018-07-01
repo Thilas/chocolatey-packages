be32331d3026c71d3b46eb0058e0bc2f8c68a9a4
Chocolatey <chocolatey@realdimensions.net>
2018-07-01 18:10:38 +0000

    AU: 1 updated - ilspy [skip ci] https://gist.github.com/c09573f2f23bc2aa904d90ff63de2cfe/d0bccd04128c226668954322e2a97aa795a64082

--- ilspy/tools/chocolateyInstall.ps1
+++ ilspy/tools/chocolateyInstall.ps1
@@ -5,9 +5,9 @@ $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 # *** Automatically filled ***
 $packageArgs = @{
     packageName   = 'ilspy'
-    url           = 'https://github.com/icsharpcode/ILSpy/releases/download/v3.2-rc/ILSpy_binaries_3.2.0.3846-rc.zip'
+    url           = 'https://github.com/icsharpcode/ILSpy/releases/download/v3.2.0/ILSpy_binaries_3.2.0.3856.zip'
     unzipLocation = $toolsDir
-    checksum      = '7ea0df6176248b09dbff0504d93505df003282d829bea5131103a761cec8d3b3'
+    checksum      = 'd1bf46034e334d456b0f7698b6493e21e664e3f3c12b1206fcf6feab8a8c6b5f'
     checksumType  = 'sha256'
 }
 # *** Automatically filled ***
