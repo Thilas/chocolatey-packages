53fe4b1da4a911254e9150e02b0330392008af5d
Thomas Démoulins <thomas.demoulins@enablon.com>
2018-07-09 20:41:04 +0200

    Fix old streams

--- strawberryperl/update.ps1
+++ strawberryperl/update.ps1
@@ -4,7 +4,7 @@ param($IncludeStream, [switch] $Force)
 . "$PSScriptRoot\..\Common.ps1"
 
 function global:au_GetLatest {
-    # stream 5.12 links seem misspelled (*64bit* instead of *32bit*)
+    # 5.10 streams and below only have 32bit installers
     return Get-LinksLatest -ReleasesUrl 'http://strawberryperl.com/releases.html' `
                            -GetVersion { param($Url)
                                if ($Url -match '-(?<Version>\d+(?:\.\d+){1,3})') { $Url = $Matches.Version }
@@ -13,8 +13,8 @@ function global:au_GetLatest {
                            -StreamFieldCount 2 `
                            -FileType 'msi' `
                            -IsLink { $_.onclick } `
-                           -IsUrl32 { param($Url, $Version) $Url -notlike '*64bit*' -or $Version.ToString(2) -eq '5.12' } `
-                           -IsUrl64 { param($Url) $Url -notlike '*32bit*' } `
+                           -IsUrl32 { param($Url) $Url -notlike '*64bit*' } `
+                           -IsUrl64 { param($Url, $Version) $Url -like '*64bit*' -or $Version -lt '5.11' } `
                            -Latest @{
                                SilentArgs              = '/qn /norestart'
                                ValidExitCodes          = '0, 3010, 1641'
