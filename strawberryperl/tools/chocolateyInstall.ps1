965df44cf46adbd92162685c6d6a05d1b580f9e2
Chocolatey <chocolatey@realdimensions.net>
2018-07-09 18:46:18 +0000

    AU: 1 updated - strawberryperl [skip ci] https://gist.github.com/c09573f2f23bc2aa904d90ff63de2cfe/5a466fb197826e0d9144c6b3fad61c04332fb741

--- strawberryperl/tools/chocolateyInstall.ps1
+++ strawberryperl/tools/chocolateyInstall.ps1
@@ -4,12 +4,12 @@
 $packageArgs = @{
     packageName    = 'strawberryperl'
     fileType       = 'msi'
-    url            = 'http://strawberryperl.com/download/5.26.2.1/strawberry-perl-5.26.2.1-32bit.msi'
-    url64bit       = 'http://strawberryperl.com/download/5.26.2.1/strawberry-perl-5.26.2.1-64bit.msi'
+    url            = 'http://strawberryperl.com/download/5.28.0.1/strawberry-perl-5.28.0.1-32bit.msi'
+    url64bit       = 'http://strawberryperl.com/download/5.28.0.1/strawberry-perl-5.28.0.1-64bit.msi'
     silentArgs     = '/qn /norestart'
-    checksum       = 'd8e47b67b7dc826348443efd2204fa55acd365a24eee7016c2d9d9167aa624be'
+    checksum       = '652c5ade0785bf6745a6c0d61de2f28ba6caf8f5859f7c19915ebc65dd095667'
     checksumType   = 'sha256'
-    checksum64     = '41ca789c86db50b0d0af64fd7bff7816a51a20d22d145535f892ad2450a8b10b'
+    checksum64     = '215fe9db4338904062065afaac9bf5cb7273c5fc6b943d0fdc60036df8755821'
     checksumType64 = 'sha256'
     validExitCodes = @(0, 3010, 1641)
 }
