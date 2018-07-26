a10f640f499d9d3e72dbd8cd92a2e3fd59edb53c
Thomas Démoulins <thomas.demoulins@enablon.com>
2018-07-26 22:28:37 +0200

    Support streams on eclipse (fix #7)

--- eclipse/update.ps1
+++ eclipse/update.ps1
@@ -4,17 +4,51 @@ param([switch] $Force)
 . "$PSScriptRoot\..\Common.ps1"
 
 function global:au_GetLatest {
-    $latest = Get-BasicLatest -ReleaseUrl 'https://www.eclipse.org/downloads/packages/' `
-                              -TagNamePattern '<a href="/downloads/packages/">Eclipse .+ \((?<TagName>[^)]+)\)</a>' `
-                              -SkipTagName `
-                              -FileType 'zip' `
-                              -IsUrl32 { param($Url) $Url -like "*jee*win32*" -and $Url -notlike '*x86_64*' } `
-                              -IsUrl64 { param($Url) $Url -like "*jee*win32*" -and $Url -like '*x86_64*' }
-    $latest.Url32 += '&r=1'
-    $latest.Url64 += '&r=1'
-    return $latest
+    return Get-Latest -ReleasesUrl 'https://www.eclipse.org/downloads/packages/' `
+                      -TagNamePattern 'Eclipse \w+ \((?<TagName>[^)]+)\)' `
+                      -LinkPattern '/downloads/packages/(release/[^/]+/(?<Build>\d+)(?<Revision>a)?)?' `
+                      -VersionCount 3 `
+                      -StreamFieldCount 2
 }
 
+function Get-Latest {
+    [CmdletBinding()]
+    param(
+        [string] $ReleasesUrl,
+        [string] $TagNamePattern, # must include a TagName capture
+        [string] $LinkPattern, # must include Build and Revision captures
+        [int] $VersionCount,
+        [int] $StreamFieldCount
+    )
+    $releases = Invoke-WebRequest -Uri $ReleasesUrl -UseBasicParsing
+
+    $links = $releases.Links | ? { $_.outerHTML -match $TagNamePattern -and $_.href -match $LinkPattern }
+    $links = $links | select -First $VersionCount
+
+    $result = [ordered] @{}
+    $links | % {
+        $_.outerHTML -match $TagNamePattern
+        $tagName = $Matches.TagName
+        $_.href -match $LinkPattern
+        if ($Matches.Build) { $tagName += '.{0}' -f $Matches.Build }
+        if ($Matches.Revision) { $tagName += '.1' }
+        $version = Get-Version $tagName
+
+        $url = Get-Url $ReleasesUrl $_.href -ForceHttps
+
+        $latest = Get-BasicLatest -ReleaseUrl $url `
+                                  -GetTagName { $version } `
+                                  -SkipTagName `
+                                  -FileType 'zip' `
+                                  -IsUrl32 { param($Url) $Url -like "*jee*win32*" -and $Url -notlike '*x86_64*' } `
+                                  -IsUrl64 { param($Url) $Url -like "*jee*win32*" -and $Url -like '*x86_64*' } `
+                                  -ForceHttps
+        $latest.Url32 += '&r=1'
+        $latest.Url64 += '&r=1'
+        $result.Add($version.ToString($StreamFieldCount), $latest)
+    }
+    return @{ Streams = $result }
+}
 function global:au_SearchReplace {
     @{
         'tools\chocolateyInstall.ps1' = @{
