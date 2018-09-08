a8c359504056eaadffa089052372055cef695d4b
Chocolatey <chocolatey@realdimensions.net>
2018-09-08 12:09:34 +0000

    AU: 1 updated - subtitleedit [skip ci] https://gist.github.com/c09573f2f23bc2aa904d90ff63de2cfe/ab07ed62764ef396c3aae57bc2697ff38cb1e9a2

--- subtitleedit/tools/chocolateyInstall.ps1
+++ subtitleedit/tools/chocolateyInstall.ps1
@@ -5,9 +5,9 @@ $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 # *** Automatically filled ***
 $packageArgs = @{
     packageName   = 'subtitleedit'
-    url           = 'https://github.com/SubtitleEdit/subtitleedit/releases/download/3.5.6/SubtitleEdit-3.5.6-Setup.zip'
+    url           = 'https://github.com/SubtitleEdit/subtitleedit/releases/download/3.5.7/SubtitleEdit-3.5.7-Setup.zip'
     unzipLocation = $toolsDir
-    checksum      = '1a35e63d703871d444f63312e9f245c2bde44a4fcbe10b533aff62d4c3d9552e'
+    checksum      = '6e2ca329890fa6194edc815fe889b9299b453383f71e31df527ab733c813468a'
     checksumType  = 'sha256'
 }
 # *** Automatically filled ***
@@ -18,7 +18,7 @@ Install-ChocolateyZipPackage @packageArgs
 $packageArgs = @{
     packageName    = 'subtitleedit'
     fileType       = 'exe'
-    file           = "$toolsDir\SubtitleEdit-3.5.6-Setup.exe"
+    file           = "$toolsDir\SubtitleEdit-3.5.7-Setup.exe"
     silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
     validExitCodes = @(0)
 }
