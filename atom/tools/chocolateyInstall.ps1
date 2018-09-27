162f98a5915411b66ac9863be7fe24ba15a4883a
Chocolatey <chocolatey@realdimensions.net>
2018-09-27 00:13:03 +0000

    AU: 1 updated - atom [skip ci] https://gist.github.com/c09573f2f23bc2aa904d90ff63de2cfe/3db6abbc4bcc93984c7e2cbe23426a577c786714

--- atom/tools/chocolateyInstall.ps1
+++ atom/tools/chocolateyInstall.ps1
@@ -4,12 +4,12 @@
 $packageArgs = @{
     packageName    = 'atom'
     fileType       = 'exe'
-    url            = 'https://github.com/atom/atom/releases/download/v1.31.0-beta0/AtomSetup.exe'
-    url64bit       = 'https://github.com/atom/atom/releases/download/v1.31.0-beta0/AtomSetup-x64.exe'
+    url            = 'https://github.com/atom/atom/releases/download/v1.32.0-beta0/AtomSetup.exe'
+    url64bit       = 'https://github.com/atom/atom/releases/download/v1.32.0-beta0/AtomSetup-x64.exe'
     silentArgs     = '--silent'
-    checksum       = '9638713f914564a24e4cc3876056dcae9cc4fdb63e97ec54c360bfc6725bcb46'
+    checksum       = '3bc65271c4462bfe93400f37edde4284b5c8a9c047e5147d379afe17b19c51b7'
     checksumType   = 'sha256'
-    checksum64     = 'fe5daf647b1aa8a52b89ba03489ebb39a3a6c1f0507546083dcd77e3d33f3a69'
+    checksum64     = '3d544a81aee287027ddbdef0fe3cf930fc84424d4b660b95588cd8ad834b6731'
     checksumType64 = 'sha256'
     validExitCodes = @(0)
 }
