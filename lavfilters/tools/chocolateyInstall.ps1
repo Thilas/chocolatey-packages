3ba63a2008e065d78af5c34e52e1c13780bd8f62
Chocolatey <chocolatey@realdimensions.net>
2018-10-05 12:09:48 +0000

    AU: 1 updated - lavfilters [skip ci] https://gist.github.com/c09573f2f23bc2aa904d90ff63de2cfe/2606d9efde930e6b0cc7a66276a8087f6c577a08

--- lavfilters/tools/chocolateyInstall.ps1
+++ lavfilters/tools/chocolateyInstall.ps1
@@ -4,9 +4,9 @@
 $packageArgs = @{
     packageName    = 'lavfilters'
     fileType       = 'exe'
-    url            = 'https://github.com/Nevcairiel/LAVFilters/releases/download/0.71/LAVFilters-0.71-Installer.exe'
+    url            = 'https://github.com/Nevcairiel/LAVFilters/releases/download/0.73/LAVFilters-0.73-Installer.exe'
     silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-    checksum       = 'fde31a07d541e06a7b24c6eac8edcd093b3674794c5f0b0baf1f0654288008b8'
+    checksum       = '9e75f3ed760d54b1e8134072971b724b4707e3eca14a90ed233ac71ab51f94ee'
     checksumType   = 'sha256'
     validExitCodes = @(0)
 }
