0cfaba0b75098e81fcd702ac66d7af17efc17132
Chocolatey <chocolatey@realdimensions.net>
2019-01-09 00:15:06 +0000

    AU: 2 updated - atom vscodium [skip ci] https://gist.github.com/c09573f2f23bc2aa904d90ff63de2cfe/adf14fedee850074eadee1e5ac08dcb104982c82

--- atom/tools/chocolateyInstall.ps1
+++ atom/tools/chocolateyInstall.ps1
@@ -3,14 +3,14 @@
 # *** Automatically filled ***
 $packageArgs = @{
     packageName    = 'atom'
-    softwareName   = ''
+    softwareName   = 'Atom'
     fileType       = 'exe'
-    url            = 'https://github.com/atom/atom/releases/download/v1.34.0-beta1/AtomSetup.exe'
-    url64bit       = 'https://github.com/atom/atom/releases/download/v1.34.0-beta1/AtomSetup-x64.exe'
+    url            = 'https://github.com/atom/atom/releases/download/v1.35.0-beta0/AtomSetup.exe'
+    url64bit       = 'https://github.com/atom/atom/releases/download/v1.35.0-beta0/AtomSetup-x64.exe'
     silentArgs     = '--silent'
-    checksum       = '53d4f5fd85737857e9c79fae1727aa81a5f3a7af10b7666b84718ad4b22bd530'
+    checksum       = 'b19442f60a9640613467ea019464ae4447ac1e6339bc7b5cef8768321ac86ae6'
     checksumType   = 'sha256'
-    checksum64     = 'd54962316223707b1d225f6d9690137a572d71fcec00f4c375cab6e5931f3d3c'
+    checksum64     = '2c9629966cbdacfa8d5f5afe6606e4ef261fc644ceb1c39842e135a080561929'
     checksumType64 = 'sha256'
     validExitCodes = @(0)
 }
--- vscodium/tools/chocolateyInstall.ps1
+++ vscodium/tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 # *** Automatically filled ***
 $softwareName = 'VSCodium'
-$version      = '1.30.1'
+$version      = '1.30.2'
 # *** Automatically filled ***
 
 if ($version -eq (Get-UninstallRegistryKey $softwareName).DisplayVersion) {
@@ -18,12 +18,12 @@ $packageArgs = @{
     packageName    = 'vscodium'
     softwareName   = $softwareName
     fileType       = 'exe'
-    url            = 'https://github.com/VSCodium/vscodium/releases/download/1.30.1/VSCodiumSetup-x64-1.30.1.exe'
-    url64bit       = 'https://github.com/VSCodium/vscodium/releases/download/1.30.1/VSCodiumSetup-x64-1.30.1.exe'
+    url            = 'https://github.com/VSCodium/vscodium/releases/download/1.30.2/VSCodiumSetup-x64-1.30.2.exe'
+    url64bit       = 'https://github.com/VSCodium/vscodium/releases/download/1.30.2/VSCodiumSetup-x64-1.30.2.exe'
     silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-    checksum       = '624d751d481f1e7bdc816ded3f5f4f9c54bd848b5766b2af73fe290c2008e530'
+    checksum       = '5144a63e7433a2525f27084e50c8b4395475083bd88db7afb613fbd202f895ce'
     checksumType   = 'sha256'
-    checksum64     = '624d751d481f1e7bdc816ded3f5f4f9c54bd848b5766b2af73fe290c2008e530'
+    checksum64     = '5144a63e7433a2525f27084e50c8b4395475083bd88db7afb613fbd202f895ce'
     checksumType64 = 'sha256'
     validExitCodes = @(0, 3010, 1641)
 }
