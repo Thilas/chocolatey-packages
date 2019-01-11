d82670f5ffaf0e909e423c57f2321327b729d7a4
Thomas Démoulins <thomas.demoulins@enablon.com>
2019-01-11 11:56:41 +0100

    Disable vscodium x86 version (for now)

--- vscodium/update.ps1
+++ vscodium/update.ps1
@@ -4,6 +4,7 @@ param([switch] $Force)
 . "$PSScriptRoot\..\Common.ps1"
 
 function global:au_GetLatest {
+    # no 32-bits version for now (see https://github.com/VSCodium/vscodium/issues/66)
     return Get-GitHubLatest -Repository 'VSCodium/vscodium' `
                             -FileType 'exe' `
                             -IsUrl32 { param($Url) $Url -like '*VSCodiumSetup-*' } `
@@ -22,11 +23,11 @@ function global:au_SearchReplace {
             "^([$]version\s*=\s*)'.*'$"           = "`$1'$($Latest.Version)'"
             "^(\s*packageName\s*=\s*)'.*'$"       = "`$1'$($Latest.PackageName)'"
             "^(\s*fileType\s*=\s*)'.*'$"          = "`$1'$($Latest.FileType)'"
-            "^(\s*url\s*=\s*)'.*'$"               = "`$1'$($Latest.Url32)'"
+            # "^(\s*url\s*=\s*)'.*'$"               = "`$1'$($Latest.Url32)'"
             "^(\s*url64bit\s*=\s*)'.*'$"          = "`$1'$($Latest.Url64)'"
             "^(\s*silentArgs\s*=\s*)'.*'$"        = "`$1'$($Latest.SilentArgs)'"
-            "^(\s*checksum\s*=\s*)'.*'$"          = "`$1'$($Latest.Checksum32)'"
-            "^(\s*checksumType\s*=\s*)'.*'$"      = "`$1'$($Latest.ChecksumType32)'"
+            # "^(\s*checksum\s*=\s*)'.*'$"          = "`$1'$($Latest.Checksum32)'"
+            # "^(\s*checksumType\s*=\s*)'.*'$"      = "`$1'$($Latest.ChecksumType32)'"
             "^(\s*checksum64\s*=\s*)'.*'$"        = "`$1'$($Latest.Checksum64)'"
             "^(\s*checksumType64\s*=\s*)'.*'$"    = "`$1'$($Latest.ChecksumType64)'"
             "^(\s*validExitCodes\s*=\s*)@\(.*\)$" = "`$1@($($Latest.ValidExitCodes))"
