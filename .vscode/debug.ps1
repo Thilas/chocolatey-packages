a40e08624ea4280382817b8488b23a3046b1751b
Thomas Démoulins <tdemoulins@gmail.com>
2019-05-14 23:03:33 +0200

    Make AU debug easier in vscode

---
+++ .vscode/debug.ps1
@@ -0,0 +1,14 @@
+﻿[CmdletBinding(PositionalBinding = $false)]
+param(
+    [Parameter(Mandatory = $true)] [string] $Path
+)
+
+$module = "$PSScriptRoot\..\..\majkinetor\au\AU"
+if (Test-Path $module) {
+    Get-Module AU | Remove-Module
+    Import-Module $module
+} else {
+    Write-Warning "AU repository not found"
+}
+
+& $Path
