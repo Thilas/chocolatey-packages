ea09a98aefb84a33e534c5f41c208a5d37bf1a82
Chocolatey <chocolatey@realdimensions.net>
2019-06-28 18:06:13 +0000

    AU: 1 updated - airserver [skip ci] https://gist.github.com/c09573f2f23bc2aa904d90ff63de2cfe/8ed81f89663e2d93050af1279bd81d60e9277515

--- airserver/tools/chocolateyInstall.ps1
+++ airserver/tools/chocolateyInstall.ps1
@@ -5,12 +5,12 @@ $packageArgs = @{
     packageName    = 'airserver'
     softwareName   = 'AirServer *'
     fileType       = 'msi'
-    url            = 'http://dl.airserver.com/pc32/AirServer-5.5.6-x86.msi'
-    url64bit       = 'http://dl.airserver.com/pc32/AirServer-5.5.6-x64.msi'
+    url            = 'http://dl.airserver.com/pc32/AirServer-5.5.7-x86.msi'
+    url64bit       = 'http://dl.airserver.com/pc32/AirServer-5.5.7-x64.msi'
     silentArgs     = '/qn /norestart'
-    checksum       = '6412141f10d8d6fb4a3bef816f120675bc15669f81ca5896f9ca10c12986724a'
+    checksum       = 'b0c3515721590a94fb5588e1ecee0daa65c1e4c88b3c2144cde36a621dc7dd67'
     checksumType   = 'sha256'
-    checksum64     = '8e026ebf42a18295355c256e186b5268d750300cf30455624d02ccddfba103c7'
+    checksum64     = '32ac65557ce4dad7baa09c47e1503a8a9d242c168f6301d27be3e426f4003bc1'
     checksumType64 = 'sha256'
     validExitCodes = @(0, 3010, 1641)
 }
