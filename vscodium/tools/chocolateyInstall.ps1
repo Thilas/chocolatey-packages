1a9cc8c45a1f0cb682d97628a6e7c8322808f24d
Chocolatey <chocolatey@realdimensions.net>
2019-09-12 12:07:57 +0000

    AU: 1 updated - vscodium [skip ci] https://gist.github.com/c09573f2f23bc2aa904d90ff63de2cfe/90dfc540c954376deef7d810bbb61903eb79fefe

--- vscodium/tools/chocolateyInstall.ps1
+++ vscodium/tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 # *** Automatically filled ***
 $softwareName = 'VSCodium'
-$version      = '1.38.0'
+$version      = '1.38.1'
 # *** Automatically filled ***
 
 if ($version -eq (Get-UninstallRegistryKey $softwareName).DisplayVersion) {
@@ -18,12 +18,12 @@ $packageArgs = @{
     packageName    = 'vscodium'
     softwareName   = $softwareName
     fileType       = 'exe'
-    url            = 'https://github.com/VSCodium/vscodium/releases/download/1.38.0/VSCodiumSetup-ia32-1.38.0.exe'
-    url64bit       = 'https://github.com/VSCodium/vscodium/releases/download/1.38.0/VSCodiumSetup-x64-1.38.0.exe'
+    url            = 'https://github.com/VSCodium/vscodium/releases/download/1.38.1/VSCodiumSetup-ia32-1.38.1.exe'
+    url64bit       = 'https://github.com/VSCodium/vscodium/releases/download/1.38.1/VSCodiumSetup-x64-1.38.1.exe'
     silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-    checksum       = 'cca016f9e329f61db652092a7484caab07dc378d40b629e74646031f3c4e3c00'
+    checksum       = '44bab4c575ca9ebddb2830798067d93093b1433dbaef1bbda709ad7214eb37e9'
     checksumType   = 'sha256'
-    checksum64     = '17567fe6e7deff0a40dbd5f40ca1b5083c4555d137d364b5ed0e9796bd6aa5ac'
+    checksum64     = '5589467729d3d07563bdb84956565b03b00b36219f512d1650d6247bcb3a21ed'
     checksumType64 = 'sha256'
     validExitCodes = @(0, 3010, 1641)
 }
