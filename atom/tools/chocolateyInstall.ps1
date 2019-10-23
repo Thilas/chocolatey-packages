2aae4f2955fb1c3de14cb45b4b7522f1b80e3e2b
Chocolatey <chocolatey@realdimensions.net>
2019-10-23 18:06:36 +0000

    AU: 1 updated - atom [skip ci] https://gist.github.com/c09573f2f23bc2aa904d90ff63de2cfe/79c5bd75ed8ded9cd9fd24c86c23dd64bf03e891

--- atom/tools/chocolateyInstall.ps1
+++ atom/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 # *** Automatically filled ***
 $softwareName = 'Atom'
-$version      = ''
+$version      = '1.42.0-beta0'
 # *** Automatically filled ***
 
 if ($version -eq (Get-UninstallRegistryKey $softwareName).DisplayVersion) {
@@ -15,13 +15,13 @@ $packageArgs = @{
     packageName    = 'atom'
     softwareName   = $softwareName
     fileType       = 'exe'
-    url            = 'https://github.com/atom/atom/releases/download/v1.41.0-beta1/AtomSetup.exe'
-    url64bit       = 'https://github.com/atom/atom/releases/download/v1.41.0-beta1/AtomSetup-x64.exe'
+    url            = 'https://github.com/atom/atom/releases/download/v1.42.0-beta0/AtomSetup.exe'
+    url64bit       = 'https://github.com/atom/atom/releases/download/v1.42.0-beta0/AtomSetup-x64.exe'
     silentArgs     = '--silent'
-    checksum       = '146b96e07f0598fcc21d75a7681b333605f728f07064f1fd86b1b9d46c314da6'
-    checksumType   = 'sha256'
-    checksum64     = '6bc4bf7f471e72d6c303b724a0523b1aa42c5e8e3d122f5cf496ffa4ea77aac6'
-    checksumType64 = 'sha256'
+    checksum       = ''
+    checksumType   = ''
+    checksum64     = ''
+    checksumType64 = ''
     validExitCodes = @(0)
 }
 # *** Automatically filled ***
