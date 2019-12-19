634c204c558f3422a2e7d358f0c19cfdf5378c45
Chocolatey <chocolatey@realdimensions.net>
2019-12-19 12:07:45 +0000

    AU: 1 updated - procexp [skip ci] https://gist.github.com/c09573f2f23bc2aa904d90ff63de2cfe/6982ac1a186eedf4a266fa8810d3c3fe913a9767

--- procexp/tools/chocolateyInstall.ps1
+++ procexp/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
     packageName    = 'procexp'
     url            = 'https://download.sysinternals.com/files/ProcessExplorer.zip'
     unzipLocation  = $toolsDir
-    checksum       = '8e7d9ca5085b1a817f16889b3c123cae9bc4e76b70c4bb02c8c823f35c0db428'
+    checksum       = '5818f8df2afdde87163f26900dc4086998bd37792c040654bc61c44ebac38626'
     checksumType   = 'sha256'
     validExitCodes = @(0)
 }
