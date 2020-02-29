a13589843e4975442600624f7f6a3ef47d07e9d0
Chocolatey <chocolatey@realdimensions.net>
2020-02-29 19:05:16 +0000

    AU: 1 updated - vscodium.portable [skip ci] https://gist.github.com/c09573f2f23bc2aa904d90ff63de2cfe/bf5d0c712e620d5092eb65fd189a85860045d593

--- vscodium.portable/tools/chocolateyInstall.ps1
+++ vscodium.portable/tools/chocolateyInstall.ps1
@@ -8,12 +8,12 @@ Write-Verbose "Installation Path: $installationPath"
 # *** Automatically filled ***
 $packageArgs = @{
     packageName    = 'vscodium.portable'
-    url            = 'https://github.com/VSCodium/vscodium/releases/download/1.42.0/VSCodium-win32-ia32-1.42.0.zip'
-    url64bit       = 'https://github.com/VSCodium/vscodium/releases/download/1.42.0/VSCodium-win32-x64-1.42.0.zip'
+    url            = 'https://github.com/VSCodium/vscodium/releases/download/1.42.1/VSCodium-win32-ia32-1.42.1.zip'
+    url64bit       = 'https://github.com/VSCodium/vscodium/releases/download/1.42.1/VSCodium-win32-x64-1.42.1.zip'
     unzipLocation  = $installationPath
-    checksum       = ''
+    checksum       = 'dcf499e81549e8ddc976cfafdde531b1e3cf6173e748b9f547347e02fbf1f0aa'
     checksumType   = 'sha256'
-    checksum64     = ''
+    checksum64     = '5816781994712d6eebfd64080601784b68d9aa2e6fff2875831b637d1c2041aa'
     checksumType64 = 'sha256'
 }
 # *** Automatically filled ***
