818d81a9496f08fe7fe7cb65596de74090c31e3a
Chocolatey <chocolatey@realdimensions.net>
2020-07-29 00:06:40 +0000

    AU: 1 updated - airserver [skip ci] https://gist.github.com/c09573f2f23bc2aa904d90ff63de2cfe/df3192f1b376aaf140301ef159c5eb3edc048fd9

--- airserver/tools/chocolateyInstall.ps1
+++ airserver/tools/chocolateyInstall.ps1
@@ -5,12 +5,12 @@ $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 # *** Automatically filled ***
 $packageArgs = @{
     packageName    = 'airserver'
-    url            = 'http://dl.airserver.com/pc32/AirServer-5.6.0-x86.msi'
-    url64bit       = 'http://dl.airserver.com/pc32/AirServer-5.6.0-x64.msi'
+    url            = 'http://dl.airserver.com/pc32/AirServer-5.6.1-x86.msi'
+    url64bit       = 'http://dl.airserver.com/pc32/AirServer-5.6.1-x64.msi'
     unzipLocation  = $toolsDir
-    checksum       = 'f46cffde2ceea25961e95ad6f6a4c5c6a2a2de5da954b64ad5fec7272cd40c60'
+    checksum       = '56842660ef74c2985160e07b9b59cd1bd811011446587d0ffda7e64ef065425a'
     checksumType   = 'sha256'
-    checksum64     = 'b02d693ac1e3cbc9247ab212331c347b5c5c96f1f9c0a6fbae57c45d0b46c0f4'
+    checksum64     = '9dcf47f2c7161f8192cde724e62e1a638adaeb2a03fbc8d1933d307f884e3d18'
     checksumType64 = 'sha256'
 }
 # *** Automatically filled ***
@@ -22,8 +22,8 @@ $packageArgs = @{
     packageName    = 'airserver'
     softwareName   = 'AirServer *'
     fileType       = 'msi'
-    file           = "$toolsDir\AirServer-5.6.0-x86.msi"
-    file64         = "$toolsDir\AirServer-5.6.0-x64.msi"
+    file           = "$toolsDir\AirServer-5.6.1-x86.msi"
+    file64         = "$toolsDir\AirServer-5.6.1-x64.msi"
     silentArgs     = '/qn /norestart'
     validExitCodes = @(0, 3010, 1641)
 }
