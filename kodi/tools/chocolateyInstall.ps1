a29564ae056da29727a47ae785f76da3e048f543
Chocolatey <chocolatey@realdimensions.net>
2020-10-24 12:06:46 +0000

    AU: 1 updated - kodi [skip ci] https://gist.github.com/c09573f2f23bc2aa904d90ff63de2cfe

--- kodi/tools/chocolateyInstall.ps1
+++ kodi/tools/chocolateyInstall.ps1
@@ -5,12 +5,12 @@ $packageArgs = @{
     packageName    = 'kodi'
     softwareName   = 'Kodi*'
     fileType       = 'exe'
-    url            = 'https://mirrors.kodi.tv/releases/windows/win32/kodi-18.8-Leia-x86.exe'
-    url64bit       = 'https://mirrors.kodi.tv/releases/windows/win64/kodi-18.8-Leia-x64.exe'
+    url            = 'https://mirrors.kodi.tv/releases/windows/win32/kodi-18.9-Leia-x86.exe'
+    url64bit       = 'https://mirrors.kodi.tv/releases/windows/win64/kodi-18.9-Leia-x64.exe'
     silentArgs     = '/S'
-    checksum       = 'e8565a07ce8f1948bafd629460d73f17ea7791da488b24b7a935c6f19f2ab511'
+    checksum       = '4b9fc49c2211b9c26190d584808cd4f89bf7ff8f7f9f8084ce8c09d538969bc1'
     checksumType   = 'sha256'
-    checksum64     = '4b19cc378e4ed0d3149b54d827ef767e6c1f4a51e08cc8e475578a70f6e4177a'
+    checksum64     = '1819c2bed5bf94b0491879c09e093a40eb358e8b0f5e024c54542d52a27fe78b'
     checksumType64 = 'sha256'
     validExitCodes = @(0)
 }
