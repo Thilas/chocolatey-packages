547d46ac40513f00b14d73f45e8e6fe228696c53
Thomas Démoulins <tdemoulins@gmail.com>
2020-11-07 12:27:34 +0100

    Unzip subtitleedit in temp location

--- subtitleedit/tools/chocolateyInstall.ps1
+++ subtitleedit/tools/chocolateyInstall.ps1
@@ -1,12 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$packageCacheLocation = Get-PackageCacheLocation
 
 # *** Automatically filled ***
 $packageArgs = @{
     packageName   = 'subtitleedit'
     url           = 'https://github.com/SubtitleEdit/subtitleedit/releases/download/3.5.17/SubtitleEdit-3.5.17-Setup.zip'
-    unzipLocation = $toolsDir
+    unzipLocation = $packageCacheLocation
     checksum      = '4c4cd3a6da19bd0f7216dd73df22263bf59111a87a98bc526390459e4f48f98c'
     checksumType  = 'sha256'
 }
@@ -19,7 +19,7 @@ $packageArgs = @{
     packageName    = 'subtitleedit'
     softwareName   = 'Subtitle Edit *'
     fileType       = 'exe'
-    file           = "$toolsDir\SubtitleEdit-3.5.17-Setup.exe"
+    file           = "$packageCacheLocation\SubtitleEdit-3.5.17-Setup.exe"
     silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
     validExitCodes = @(0)
 }
@@ -27,5 +27,5 @@ $packageArgs = @{
 
 Install-ChocolateyInstallPackage @packageArgs
 
-$file = $packageArgs.file
-Remove-Item $file, "$file.ignore" -Force -ErrorAction SilentlyContinue
+$logPath = Join-Path $Env:ChocolateyPackageFolder '*.zip.txt'
+Remove-Item $logPath, $packageArgs.file -Force -ErrorAction SilentlyContinue
