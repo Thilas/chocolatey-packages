b745ef99369ad03a90ebf20f5aa0ba64c50b703d
Chocolatey <chocolatey@realdimensions.net>
2021-04-22 00:06:22 +0000

    AU: 1 updated - procmon [skip ci] https://gist.github.com/c09573f2f23bc2aa904d90ff63de2cfe/7b8ea6601510773d0d50bef2541a65d867ae9894

--- procmon/tools/chocolateyInstall.ps1
+++ procmon/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
     packageName    = 'procmon'
     url            = 'https://download.sysinternals.com/files/ProcessMonitor.zip'
     unzipLocation  = $toolsDir
-    checksum       = 'eb24c5bd418f0d7d9289e27310e2af87a8c0e79c3a953cd22cdc1382e5dfe7cb'
+    checksum       = '434024aad97ad50fbcceed982369460da2a69305127c3d38010c0e4821c8bbda'
     checksumType   = 'sha256'
     validExitCodes = @(0)
 }
