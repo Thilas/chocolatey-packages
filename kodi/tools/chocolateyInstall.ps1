6678568d56c4f86a6c3ec03c3448db3a269dc179
Chocolatey <chocolatey@realdimensions.net>
2022-03-03 18:06:45 +0000

    AU: 1 updated - kodi [skip ci] https://gist.github.com/c09573f2f23bc2aa904d90ff63de2cfe/49fbe0abf5962dffdd213225d6328440ed20ee06

--- kodi/tools/chocolateyInstall.ps1
+++ kodi/tools/chocolateyInstall.ps1
@@ -5,12 +5,12 @@ $packageArgs = @{
     packageName    = 'kodi'
     softwareName   = 'Kodi*'
     fileType       = 'exe'
-    url            = 'https://mirrors.kodi.tv/releases/windows/win32/kodi-19.3-Matrix-x86.exe'
-    url64bit       = 'https://mirrors.kodi.tv/releases/windows/win64/kodi-19.3-Matrix-x64.exe'
+    url            = 'https://mirrors.kodi.tv/releases/windows/win32/kodi-19.4-Matrix-x86.exe'
+    url64bit       = 'https://mirrors.kodi.tv/releases/windows/win64/kodi-19.4-Matrix-x64.exe'
     silentArgs     = '/S'
-    checksum       = '8915d4a47c27e513b14f09f134c8195edc46e1bcd5fcf3585cb78afc5db96d6f'
+    checksum       = '0df9b3d6a75b33af2dfe94addd926cab6eea10c2d04200e61b782944c36a5ad5'
     checksumType   = 'sha256'
-    checksum64     = '0f75ddfca90fa685b9ae5eb4f6d315b58ad7673d44937a9a399f0c5ea29a3075'
+    checksum64     = '9988067ed7c9bc64258fa9553dbb44d6619443ee10fd2127b2555da795eb2a69'
     checksumType64 = 'sha256'
     validExitCodes = @(0)
 }
