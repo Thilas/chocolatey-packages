5239cb6331b04d69b99efa3f55d8702bc6217c63
Chocolatey <chocolatey@realdimensions.net>
2022-05-29 18:06:11 +0000

    AU: 1 updated - sabnzbd [skip ci] https://gist.github.com/c09573f2f23bc2aa904d90ff63de2cfe/d957eb72e3eba7d9958781244354e43336a42b6e

--- sabnzbd/tools/chocolateyInstall.ps1
+++ sabnzbd/tools/chocolateyInstall.ps1
@@ -7,9 +7,9 @@ $packageArgs = @{
     packageName    = 'sabnzbd'
     softwareName   = 'SABnzbd*'
     fileType       = 'exe'
-    url            = 'https://github.com/sabnzbd/sabnzbd/releases/download/3.6.0RC2/SABnzbd-3.6.0RC2-win-setup.exe'
+    url            = 'https://github.com/sabnzbd/sabnzbd/releases/download/3.6.0RC3/SABnzbd-3.6.0RC3-win-setup.exe'
     silentArgs     = '/S'
-    checksum       = 'c3f381c2d8f027b3abfea7c1b8068696ce65bfcfcb05d286602febc8669c0296'
+    checksum       = '6c3d7036247ca9897f6ebb4d3aac08dfd280e6e73f8ecd3091c7fb0b939a46e7'
     checksumType   = 'sha256'
     validExitCodes = @(0)
 }
