4aa1ffcc7f547e5487a816155a384b0342e063cd
Chocolatey <chocolatey@realdimensions.net>
2022-08-16 18:06:29 +0000

    AU: 3 updated - vscodium vscodium.install vscodium.portable [skip ci] https://gist.github.com/c09573f2f23bc2aa904d90ff63de2cfe/dce01aa871b63b624a28be8897632e9125a68d0a

--- vscodium.install/tools/chocolateyInstall.ps1
+++ vscodium.install/tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 # *** Automatically filled ***
 $softwareName = 'VSCodium'
-$version      = '1.70.1'
+$version      = '1.70.1.22228'
 # *** Automatically filled ***
 
 if (!$Env:ChocolateyForce -and $version -eq (Get-UninstallRegistryKey $softwareName).DisplayVersion) {
@@ -18,12 +18,12 @@ $packageArgs = @{
     packageName    = 'vscodium.install'
     softwareName   = $softwareName
     fileType       = 'exe'
-    url            = 'https://github.com/VSCodium/vscodium/releases/download/1.70.1/VSCodiumSetup-ia32-1.70.1.exe'
-    url64bit       = 'https://github.com/VSCodium/vscodium/releases/download/1.70.1/VSCodiumSetup-x64-1.70.1.exe'
+    url            = 'https://github.com/VSCodium/vscodium/releases/download/1.70.1.22228/VSCodiumSetup-ia32-1.70.1.22228.exe'
+    url64bit       = 'https://github.com/VSCodium/vscodium/releases/download/1.70.1.22228/VSCodiumSetup-x64-1.70.1.22228.exe'
     silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-    checksum       = 'f02ea4f25645d6df003d81dab3c9d4139841118bf9c82fa473cd3299ce5ed57a'
+    checksum       = '39bc9d50884bcfb85b64968aa0ace5b5482c37f5df7c22bcfcfdc3904b50bf91'
     checksumType   = 'sha256'
-    checksum64     = '075d11c46ec1dd65768a766a520a9cd6b2e5aca50ba5e6dee80f836b90fbe85a'
+    checksum64     = '29b619a1463415c04b384d614ac70b60ef408115329be5e0528415e286b9876b'
     checksumType64 = 'sha256'
     validExitCodes = @(0, 3010, 1641)
 }
--- vscodium.portable/tools/chocolateyInstall.ps1
+++ vscodium.portable/tools/chocolateyInstall.ps1
@@ -19,12 +19,12 @@ Write-Verbose "Installation Path: $installationPath"
 # *** Automatically filled ***
 $packageArgs = @{
     packageName    = 'vscodium.portable'
-    url            = 'https://github.com/VSCodium/vscodium/releases/download/1.70.1/VSCodium-win32-ia32-1.70.1.zip'
-    url64bit       = 'https://github.com/VSCodium/vscodium/releases/download/1.70.1/VSCodium-win32-x64-1.70.1.zip'
+    url            = 'https://github.com/VSCodium/vscodium/releases/download/1.70.1.22228/VSCodium-win32-ia32-1.70.1.22228.zip'
+    url64bit       = 'https://github.com/VSCodium/vscodium/releases/download/1.70.1.22228/VSCodium-win32-x64-1.70.1.22228.zip'
     unzipLocation  = $installationPath
-    checksum       = 'e18ec423d3891ad3fca4a611b289b96a8064aa0f4064191cd4c1b75c7194b38c'
+    checksum       = 'cd61b8d9cd26e7245380c3508dac900ed418d4eea7a1702d0622e34b9f73c18f'
     checksumType   = 'sha256'
-    checksum64     = '0a3085d26c5fb76d74fe616eec877e2362e1592b43ee40358479780e64ed1249'
+    checksum64     = 'b96a675415384cad41489b37bcf66a661e382c0ea6dcc1d4f34308765d626d8f'
     checksumType64 = 'sha256'
 }
 # *** Automatically filled ***
