22f3b402ad8a3d6cf8739f02ca07289e67af1bef
Thomas Démoulins <tdemoulins@gmail.com>
2022-09-18 12:36:04 +0200

    Fix

--- test_all.ps1
+++ test_all.ps1
@@ -1,33 +1,39 @@
-﻿#Name can be 'random N' to randomly force the Nth group of packages.
-
-param(
-    [string[]] $Name,
-    [string] $Root = $PSScriptRoot,
+﻿param(
+    [string[]] $Name, # Name can be 'random N' to randomly force the Nth group of packages
+    [string] $Root = $PSScriptRoot, # Path to the AU packages
     [switch] $ThrowOnErrors
 )
 
-if (Test-Path $PSScriptRoot/update_vars.ps1) { . $PSScriptRoot/update_vars.ps1 }
-$global:au_Root = Resolve-Path $Root                        #Path to the AU packages
+if (Test-Path "$PSScriptRoot/update_vars.ps1") { . "$PSScriptRoot/update_vars.ps1" }
+$global:au_Root = Resolve-Path $Root
 
-if (($Name.Length -gt 0) -and ($Name[0] -match '^random (.+)')) {
-    [array] $lsau = lsau
+if ($Name.Length -eq 1 -and $Name[0] -match '^random (.+)') {
+    [array] $packages = Get-AuPackages
 
     $group = [int] $Matches[1]
-    $n = (Get-Random -Maximum $group)
+    $n = Get-Random -Maximum $group
     Write-Host "TESTING GROUP $($n+1) of $group"
 
-    $group_size = [int]($lsau.Count / $group) + 1
-    $Name = $lsau | select -First $group_size -Skip ($group_size*$n) | % { $_.Name }
+    $group_size = [int] ($packages.Count / $group) + 1
+    $Name = $packages | Select-Object -First $group_size -Skip ($group_size * $n) | ForEach-Object Name
 
     Write-Host ($Name -join ' ')
     Write-Host ('-'*80)
 }
 
-$options = [ordered]@{
-    Force         = $true                                   #Force all packages
-    Threads       = 10                                      #Number of background jobs to use
-    Push          = $false                                  #Push to chocolatey
-    IgnoreOn      = @(                                      #Error message parts to set the package ignore status
+$buildUrl = if ($GitHubAction) {
+    "https://github.com/Thilas/chocolatey-packages/actions/workflows/main.yml"
+} else {
+    "https://ci.appveyor.com/project/Thilas/chocolatey-packages-ure9y"
+}
+$gist_token = if ($Env:gist_token) { $Env:gist_token } else { $Env:github_api_key }
+
+$options = [ordered] @{
+    Force         = $true                                   # Force all packages
+    Threads       = 10                                      # Number of background jobs to use
+    Push          = $false                                  # Push to chocolatey
+
+    IgnoreOn = @(                                           # Error message parts to set the package ignore status
       'Could not create SSL/TLS secure channel'
       'Could not establish trust relationship'
       'The operation has timed out'
@@ -35,16 +41,16 @@ $options = [ordered]@{
       'Service Temporarily Unavailable'
       'The connection was closed unexpectedly.'
       'package version already exists'
-      'already exists on a Simple OData Server'             # https://github.com/chocolatey/chocolatey.org/issues/613
+      'already exists on a Simple OData Server'             #   https://github.com/chocolatey/chocolatey.org/issues/613
       'Conflict'
-      'A system shutdown has already been scheduled'        # https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f#wps-office-free
+      'A system shutdown has already been scheduled'        #   https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f#wps-office-free
     )
-    RepeatOn      = @(                                      #Error message parts on which to repeat package updater
-      'Could not create SSL/TLS secure channel'             # https://github.com/chocolatey/chocolatey-coreteampackages/issues/718
-      'Could not establish trust relationship'              # -||-
+    RepeatOn = @(                                           # Error message parts on which to repeat package updater
+      'Could not create SSL/TLS secure channel'             #   https://github.com/chocolatey/chocolatey-coreteampackages/issues/718
+      'Could not establish trust relationship'              #   -||-
       'Unable to connect'
       'The remote name could not be resolved'
-      'Choco pack failed with exit code 1'                  # https://github.com/chocolatey/chocolatey-coreteampackages/issues/721
+      'Choco pack failed with exit code 1'                  #   https://github.com/chocolatey/chocolatey-coreteampackages/issues/721
       'The operation has timed out'
       'Internal Server Error'
       'An exception occurred during a WebClient request'
@@ -52,33 +58,40 @@ $options = [ordered]@{
       'The connection was closed unexpectedly.'
       'Job returned no object, Vector smash ?'
     )
-    RepeatSleep   = 60                                      #How much to sleep between repeats in seconds, by default 0
-    RepeatCount   = 2                                       #How many times to repeat on errors, by default 1
+    RepeatSleep = 60                                        # How much to sleep between repeats in seconds, by default 0
+    RepeatCount = 2                                         # How many times to repeat on errors, by default 1
 
     Report = @{
-        Type = 'markdown'                                   #Report type: markdown or text
-        Path = "$PSScriptRoot\Update-Force-Test-${n}.md"    #Path where to save the report
-        Params= @{                                          #Report parameters:
-            Github_UserRepo = $Env:github_user_repo         #  Markdown: shows user info in upper right corner
-            NoAppVeyor  = $true                             #  Markdown: do not show AppVeyor build shield
-            Title       = "Update Force Test - Group ${n}"
-            UserMessage = "[Ignored](#ignored) | [Update report](https://gist.github.com/$Env:gist_id) | [Build](https://ci.appveyor.com/project/Thilas/chocolatey-packages-ure9y) | **USING AU NEXT VERSION**"       #  Markdown, Text: Custom user message to show
+        Type = 'markdown'                                   # Report type: markdown or text
+        Path = "$PSScriptRoot\Update-Force-Test-${n}.md"    # Path where to save the report
+        Params = @{                                         # Report parameters:
+            Github_UserRepo = $Env:github_user_repo         #   Markdown: shows user info in upper right corner
+            NoAppVeyor  = $true                             #   Markdown: do not show AppVeyor build shield
+            Title       = "Update Force Test - Group ${n}"  #   Markdown, Text: Title of the report, by default 'Update-AUPackages'
+            UserMessage = @(                                #   Markdown, Text: Custom user message to show
+                "[Ignored](#ignored)"
+                "[Update report](https://gist.github.com/$Env:gist_id)"
+                "[Build]($buildUrl)"
+                "**USING AU NEXT VERSION**"
+            ) -join " | "
+            NoIcons     = $false                            #   Markdown: don't show icon
+            IconSize    = 32                                #   Markdown: icon size
         }
     }
 
-    Gist = @{
-        Id     = $Env:gist_id_test                          #Your gist id; leave empty for new private or anonymous gist
-        ApiKey = $Env:github_api_key                        #Your github api key - if empty anoymous gist is created
-        Path   = "$PSScriptRoot\Update-Force-Test-${n}.md"  #List of files to add to the gist
+    Gist = if ($Env:au_noGist -ne 'true') { @{
+        Id     = $Env:gist_id_test                          # Your gist id; leave empty for new private or anonymous gist
+        ApiKey = $gist_token                                # Your github api key - if empty anoymous gist is created
+        Path   = "$PSScriptRoot\Update-Force-Test-${n}.md"  # List of files to add to the gist
         Description = "Update Force Test Report #powershell #chocolatey"
-    }
+    } } else {}
 }
 
 # Set TLS1.2
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor "Tls12"
 
-$global:info = updateall -Name $Name -Options $options
+$global:info = Update-AuPackages -Name $Name -Options $options
 
-if ($ThrowOnErrors -and ($global:info | ? Error)) {
-    throw 'Errors during test'
+if ($ThrowOnErrors -and ($global:info | Where-Object Error)) {
+    Write-Error 'Errors during test'
 }
