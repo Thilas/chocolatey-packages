ac2c5eeabde42818fde726faffea35877e066671
Thomas Démoulins <tdemoulins@gmail.com>
2022-09-24 16:31:20 +0200

    ci: Improve error management

--- test_all.ps1
+++ test_all.ps1
@@ -33,33 +33,37 @@ $gist_token = if ($Env:gist_token) { $Env:gist_token } else { $Env:github_api_ke
 
 $options = [ordered] @{
     Force         = $true                                   # Force all packages
+    Timeout       = 120                                     # Connection timeout in seconds
+    UpdateTimeout = 300                                     # Update timeout in seconds
     Threads       = 10                                      # Number of background jobs to use
     Push          = $false                                  # Push to chocolatey
 
     IgnoreOn = @(                                           # Error message parts to set the package ignore status
-      'Could not create SSL/TLS secure channel'
-      'Could not establish trust relationship'
-      'The operation has timed out'
-      'Internal Server Error'
-      'Service Temporarily Unavailable'
-      'The connection was closed unexpectedly.'
-      'package version already exists'
-      'already exists on a Simple OData Server'             #   https://github.com/chocolatey/chocolatey.org/issues/613
-      'Conflict'
-      'A system shutdown has already been scheduled'        #   https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f#wps-office-free
+        'Could not create SSL/TLS secure channel'
+        'Could not establish trust relationship'
+        'The operation has timed out'
+        'Internal Server Error'
+        'Service Temporarily Unavailable'
+        'The connection was closed unexpectedly'
+        'package version already exists'
+        'already exists on a Simple OData Server'           #   https://github.com/chocolatey/chocolatey.org/issues/613
+        'Conflict'
+        'A system shutdown has already been scheduled'      #   https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f#wps-office-free
+        'Choco pack failed with exit code 1'
     )
     RepeatOn = @(                                           # Error message parts on which to repeat package updater
-      'Could not create SSL/TLS secure channel'             #   https://github.com/chocolatey/chocolatey-coreteampackages/issues/718
-      'Could not establish trust relationship'              #   -||-
-      'Unable to connect'
-      'The remote name could not be resolved'
-      'Choco pack failed with exit code 1'                  #   https://github.com/chocolatey/chocolatey-coreteampackages/issues/721
-      'The operation has timed out'
-      'Internal Server Error'
-      'An exception occurred during a WebClient request'
-      'remote session failed with an unexpected state'
-      'The connection was closed unexpectedly.'
-      'Job returned no object, Vector smash ?'
+        'Could not create SSL/TLS secure channel'           #   https://github.com/chocolatey/chocolatey-coreteampackages/issues/718
+        'Could not establish trust relationship'            #   -||-
+        'Unable to connect'
+        'The remote name could not be resolved'
+        'Choco pack failed with exit code 1'                #   https://github.com/chocolatey/chocolatey-coreteampackages/issues/721
+        'The operation has timed out'
+        'Internal Server Error'
+        'An exception occurred during a WebClient request'
+        'remote session failed with an unexpected state'
+        'The connection was closed unexpectedly'
+        "Can't validate URL"
+        'Job returned no object, Vector smash ?'
     )
     RepeatSleep = 60                                        # How much to sleep between repeats in seconds, by default 0
     RepeatCount = 2                                         # How many times to repeat on errors, by default 1
@@ -90,7 +94,7 @@ $options = [ordered] @{
     } } else {}
 }
 
-# Set TLS1.2
+# Enable TLS1.2
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor "Tls12"
 
 $global:info = Update-AuPackages -Name $Name -Options $options
--- update_all.ps1
+++ update_all.ps1
@@ -13,45 +13,47 @@ $global:au_Root = Resolve-Path $Root
 
 $report_path  = "$PSScriptRoot\Update-AUPackages.md"
 $history_path = "$PSScriptRoot\Update-History.md"
+$update_info  = "$PSScriptRoot\update_info.xml"
 
 $gist_token = if ($Env:gist_token) { $Env:gist_token } else { $Env:github_api_key }
 
 $options = [ordered] @{
     WhatIf        = $au_WhatIf                              # Whatif all packages
     Force         = $Force                                  # Force all packages
-    Timeout       = 100                                     # Connection timeout in seconds
-    UpdateTimeout = 1200                                    # Update timeout in seconds
+    Timeout       = 120                                     # Connection timeout in seconds
+    UpdateTimeout = 300                                     # Update timeout in seconds
     Threads       = 10                                      # Number of background jobs to use
     Push          = $Env:au_Push -eq 'true'                 # Push to chocolatey
     PushAll       = $true                                   # Allow to push multiple packages at once
-    PluginPath    = ''                                      # Path to user plugins
+    # PluginPath    = ''                                      # Path to user plugins
 
     IgnoreOn = @(                                           # Error message parts to set the package ignore status
-      'Could not create SSL/TLS secure channel'
-      'Could not establish trust relationship'
-      'The operation has timed out'
-      'Internal Server Error'
-      'Service Temporarily Unavailable'
-      'The connection was closed unexpectedly.'
-      'package version already exists'
-      'already exists on a Simple OData Server'             #   https://github.com/chocolatey/chocolatey.org/issues/613
-      'Conflict'
-      'A system shutdown has already been scheduled'        #   https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f#wps-office-free
+        'Could not create SSL/TLS secure channel'
+        'Could not establish trust relationship'
+        'The operation has timed out'
+        'Internal Server Error'
+        'Service Temporarily Unavailable'
+        'The connection was closed unexpectedly'
+        'package version already exists'
+        'already exists on a Simple OData Server'           #   https://github.com/chocolatey/chocolatey.org/issues/613
+        'Conflict'
+        'A system shutdown has already been scheduled'      #   https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f#wps-office-free
     )
     RepeatOn = @(                                           # Error message parts on which to repeat package updater
-      'Could not create SSL/TLS secure channel'             #   https://github.com/chocolatey/chocolatey-coreteampackages/issues/718
-      'Could not establish trust relationship'              #   -||-
-      'Unable to connect'
-      'The remote name could not be resolved'
-      'Choco pack failed with exit code 1'                  #   https://github.com/chocolatey/chocolatey-coreteampackages/issues/721
-      'The operation has timed out'
-      'Internal Server Error'
-      'An exception occurred during a WebClient request'
-      'remote session failed with an unexpected state'
-      'The connection was closed unexpectedly.'
+        'Could not create SSL/TLS secure channel'           #   https://github.com/chocolatey/chocolatey-coreteampackages/issues/718
+        'Could not establish trust relationship'            #   -||-
+        'Unable to connect'
+        'The remote name could not be resolved'
+        'Choco pack failed with exit code 1'                #   https://github.com/chocolatey/chocolatey-coreteampackages/issues/721
+        'The operation has timed out'
+        'Internal Server Error'
+        'An exception occurred during a WebClient request'
+        'remote session failed with an unexpected state'
+        "Can't validate URL"
+        'The connection was closed unexpectedly'
     )
-    # RepeatSleep = 60                                        # How much to sleep between repeats in seconds, by default 0
-    # RepeatCount = 2                                         # How many times to repeat on errors, by default 1
+    RepeatSleep = 60                                        # How much to sleep between repeats in seconds, by default 0
+    RepeatCount = 2                                         # How many times to repeat on errors, by default 1
 
     Report = @{
         Type = 'markdown'                                   # Report type: markdown or text
@@ -96,7 +98,7 @@ $options = [ordered] @{
 
     RunInfo = @{
         Exclude = 'password', 'apikey', 'apitoken'          # Option keys which contain those words will be removed
-        Path    = "$PSScriptRoot\update_info.xml"           # Path where to save the run info
+        Path    = $update_info                              # Path where to save the run info
     }
 
     Mail = if ($Env:mail_user -and $Env:mail_pass) { @{
@@ -106,7 +108,7 @@ $options = [ordered] @{
         Password    = $Env:mail_pass
         Port        = $Env:mail_port
         EnableSsl   = $Env:mail_enablessl -eq 'true'
-        Attachment  = "$PSScriptRoot\update_info.xml"
+        Attachment  = $update_info
         UserMessage = "Update status: https://gist.github.com/$Env:gist_id"
         SendAlways  = $false                                # Send notifications every time
     } } else {}
@@ -125,7 +127,7 @@ $options = [ordered] @{
     }
 }
 
-# Set TLS1.2
+# Enable TLS1.2
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor "Tls12"
 
 if ($ForcedPackages) { Write-Host "FORCED PACKAGES: $ForcedPackages" }
