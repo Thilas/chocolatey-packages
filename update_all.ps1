999302bc635954317034007b65b35a9917486e75
Thomas Démoulins <tdemoulins@gmail.com>
2022-10-27 08:15:57 +0200

    fix(procmon): retry/ignore a chocolatey-related false positives

--- update_all.ps1
+++ update_all.ps1
@@ -38,6 +38,7 @@ $options = [ordered] @{
         'already exists on a Simple OData Server'           #   https://github.com/chocolatey/chocolatey.org/issues/613
         'Conflict'
         'A system shutdown has already been scheduled'      #   https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f#wps-office-free
+        "The term 'Write-FunctionCallLogMessage' is not recognized as a name of a cmdlet, function, script file, or executable program"
     )
     RepeatOn = @(                                           # Error message parts on which to repeat package updater
         'Could not create SSL/TLS secure channel'           #   https://github.com/chocolatey/chocolatey-coreteampackages/issues/718
@@ -52,6 +53,7 @@ $options = [ordered] @{
         "Can't validate URL"
         'Oops, something went wrong'
         'The connection was closed unexpectedly'
+        "The term 'Write-FunctionCallLogMessage' is not recognized as a name of a cmdlet, function, script file, or executable program"
     )
     RepeatSleep = 60                                        # How much to sleep between repeats in seconds, by default 0
     RepeatCount = 2                                         # How many times to repeat on errors, by default 1
