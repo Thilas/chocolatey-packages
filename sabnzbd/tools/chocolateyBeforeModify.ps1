8a2af9697d23be41d34e16d0c208a4c4b31f2b03
Thomas Démoulins <tdemoulins@gmail.com>
2022-12-03 00:34:24 +0100

    fix(sabnzbd): Service was no longer installed

--- sabnzbd/tools/chocolateyBeforeModify.ps1
+++ sabnzbd/tools/chocolateyBeforeModify.ps1
@@ -7,22 +7,16 @@ $packageName  = 'sabnzbd'
 $softwareName = 'SABnzbd*'
 # *** Automatically filled ***
 
-Write-Host 'Stopping services if they''re running...'
-Stop-Service '*' -Include 'SABnzbd', 'SABhelper' -Force
-
 . "$toolsDir\Get-InstallPath.ps1"
 $installPath = Get-InstallPath $packageName $softwareName
 
 if ($installPath) {
-    Push-Location $installPath
+    Write-Host "Stopping services..."
+    Stop-Service '*' -Include 'SABnzbd', 'SABhelper' -Force
 
     Write-Host 'Uninstalling services...'
-
-    $service = 'SABnzbd-service.exe'
-    if (Test-Path $service) { & ".\$service" remove }
-
-    $helper = 'SABnzbd-helper.exe'
-    if (Test-Path $helper) { & ".\$helper" remove }
-
-    Pop-Location
+    'SABnzbd-console.exe', 'SABnzbd-service.exe', 'SABnzbd-helper.exe' `
+    | ForEach-Object { Join-Path $installPath $_ } `
+    | Where-Object { Test-Path $_ } `
+    | ForEach-Object { & $_ remove }
 }
--- sabnzbd/tools/chocolateyInstall.ps1
+++ sabnzbd/tools/chocolateyInstall.ps1
@@ -21,26 +21,9 @@ Install-ChocolateyPackage @packageArgs
 $installPath = Get-InstallPath $packageArgs.packageName $packageArgs.softwareName
 
 if ($installPath) {
-    Push-Location $installPath
-    try {
-        Write-Host 'Installing services...'
-
-        $helper = 'SABnzbd-helper.exe'
-        if (Test-Path $helper) {
-            & ".\$helper" install
-            Set-Service 'SABhelper' -StartupType Automatic
-        }
-
-        $service = 'SABnzbd-service.exe'
-        if (Test-Path $service) {
-            $iniPath = Join-Path $Env:LOCALAPPDATA 'sabnzbd\sabnzbd.ini'
-            & ".\$service" -f $iniPath install
-            Set-Service 'SABnzbd' -StartupType Automatic
-        }
-    } finally {
-        Pop-Location
-    }
+    $iniPath = Join-Path $Env:LOCALAPPDATA 'sabnzbd\sabnzbd.ini'
+    & "$installPath\SABnzbd-console.exe" -f $iniPath install
+    Set-Service 'SABnzbd' -StartupType Automatic
+    Write-Host 'Starting service...'
+    Start-Service 'SABnzbd'
 }
-
-Write-Host 'Starting services...'
-Start-Service '*' -Include 'SABnzbd', 'SABhelper'
