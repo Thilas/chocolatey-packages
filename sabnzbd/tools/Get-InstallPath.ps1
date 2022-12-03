4c30c8092a1bad94b7e904079a33e8cddf5a5524
Thomas Démoulins <tdemoulins@gmail.com>
2022-12-03 14:50:09 +0100

    fix(sabnzbd): Service install/uninstall

--- sabnzbd/tools/Get-InstallPath.ps1
+++ sabnzbd/tools/Get-InstallPath.ps1
@@ -8,22 +8,28 @@
     [array] $key = Get-UninstallRegistryKey -SoftwareName $SoftwareName
 
     if ($key.Count -eq 1) {
+        function Get-File ([string] $path) {
+            # Remove quotes and trailing arguments if any
+            if (!$path) { return }
+            if (Test-Path $path) { return $path }
+            function Split-CommandLine ([string] $path) { $path }
+            $path = Invoke-Expression "Split-CommandLine $path"
+            if (Test-Path $path) { return $path }
+        }
         $key | ForEach-Object {
-            $file = $_.UninstallString
-            if ($file) { $file = $file.Replace('"', '') }
-            $file = Split-Path $file
-
-            if ($file -and (Test-Path $file)) {
-                return $file
+            $file = Get-File $_.UninstallString
+            $path = if ($file) { Split-Path $file }
+            if ($path) {
+                return $path
             } else {
-                Write-Warning "$PackageName install not found: $file"
+                Write-Warning "$PackageName install not found: $($_.UninstallString)"
             }
         }
     } elseif ($key.Count -eq 0) {
         Write-Warning "$PackageName install not found."
-    } elseif ($key.Count -gt 1) {
-        Write-Warning "$key.Count matches found!"
-        Write-Warning "Please alert package maintainer the following keys were matched:"
-        $key | ForEach-Object { Write-Warning "- $_.DisplayName" }
+    } else {
+        Write-Warning "$($key.Count) matches found for $PackageName!"
+        Write-Warning "Please contact package maintainer the following keys were matched:"
+        $key | ForEach-Object { Write-Warning "- $($_.DisplayName)" }
     }
 }
