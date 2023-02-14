8186b574f111fc10eeb25740cf287505f43e3689
Thomas Démoulins <tdemoulins@gmail.com>
2023-02-14 08:57:53 +0100

    Update helpers.ps1

--- subtitleedit/tools/helpers.ps1
+++ subtitleedit/tools/helpers.ps1
@@ -1,13 +1,13 @@
 function Get-MergeTasks([hashtable] $pp) {
-    $merge_tasks = @()
-    $merge_tasks += '!'*$pp.NoDesktopIcon      + 'desktopicon'
-    $merge_tasks += '!'*$pp.NoDesktopIcon      + 'desktopicon\user'
-    $merge_tasks += '!'*$pp.NoDesktopIcon      + 'desktopicon\common'
-    $merge_tasks += '!'*$pp.NoQuicklaunchIcon  + 'quicklaunchicon'
-    $merge_tasks += '!'*$pp.NoSetFileTypes     + 'associate_common'
-    $merge_tasks += '!'*!$pp.ResetDictionaries + 'reset_dictionaries'
-    $merge_tasks += '!'*!$pp.ResetSettings     + 'reset_settings'
+    $tasks = @()
+    $tasks += '!'* $pp.NoDesktopIcon     + 'desktopicon'
+    $tasks += '!'* $pp.NoDesktopIcon     + 'desktopicon\user'
+    $tasks += '!'* $pp.NoDesktopIcon     + 'desktopicon\common'
+    $tasks += '!'* $pp.NoQuicklaunchIcon + 'quicklaunchicon'
+    $tasks += '!'* $pp.NoSetFileTypes    + 'associate_common'
+    $tasks += '!'*!$pp.ResetDictionaries + 'reset_dictionaries'
+    $tasks += '!'*!$pp.ResetSettings     + 'reset_settings'
 
-    Write-Host "Merge Tasks: $merge_tasks"
-    return $merge_tasks -join ','
+    Write-Host "Merge Tasks: $tasks"
+    return $tasks -join ','
 }
