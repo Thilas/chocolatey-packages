afe36d08be8304aec4eb3259c1d6cdbec85ad86e
Thomas Démoulins <tdemoulins@gmail.com>
2023-06-18 01:26:10 +0200

    Fix kodi version

--- kodi/update.ps1
+++ kodi/update.ps1
@@ -9,7 +9,7 @@ function global:au_GetLatest {
         -FileType 'exe' `
         -GetVersion { param($Link)
             $versionPattern = '-(?<Version>\d+(?:\.\d+){1,3})-'
-            $value = if ($Link.href -match "$versionPattern.+_(?<Prerelease>[0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+| \d+)*)-") {
+            $value = if ($Link.href -match "$versionPattern.+_(?<Prerelease>[0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+| +\d+)*)-") {
                 $prerelease = $Matches.Prerelease
                 '{0}-{1}' -f $Matches.Version, $prerelease
             } elseif ($Link.href -match $versionPattern) {
