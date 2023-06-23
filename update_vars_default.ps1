1093e09aaee108d4f19b8ebd9781622683b61cf8
Thomas Démoulins <tdemoulins@gmail.com>
2023-06-23 19:46:37 +0200

    Better CI

--- update_vars_default.ps1
+++
@@ -1,7 +0,0 @@
-﻿# AU Packages Template: https://github.com/majkinetor/au-packages-template
-# Copy this file to update_vars.ps1 and set the variables there. Do not include it in the repository.
-
-$Env:api_key          = ''      # Chocolatey api key
-$Env:github_user_repo = ''      # {github_user}/{repo}
-$Env:github_api_key   = ''      # Github personal access token
-$Env:au_Push          = 'false' # Push to chocolatey
