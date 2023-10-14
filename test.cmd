d189fb40cb95b6f5b14a6b1055b9e8db353f2bb0
Thomas Démoulins <tdemoulins@gmail.com>
2023-10-14 14:40:04 +0200

    Fix CI when no packages are updated

---
+++ test.cmd
@@ -0,0 +1,4 @@
+
+choco install -y -r ppp --version vvv --source "'C:\Users\Thomas\Downloads;https://chocolatey.org/api/v2/'" --force
+choco upgrade -y -r ppp --version vvv --source "'C:\Users\Thomas\Downloads;https://chocolatey.org/api/v2/'" --force
+choco uninstall -y -r ppp
