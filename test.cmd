e4fc59e30ac50ac60a76b5e5303f8de211ebe797
Thomas Démoulins <tdemoulins@gmail.com>
2023-10-31 10:47:57 +0100

    fix

--- test.cmd
+++ test.cmd
@@ -1,4 +1,4 @@
 
-choco install -y -r ppp --version vvv --source "'C:\Users\Thomas\Downloads;https://chocolatey.org/api/v2/'" --force
-choco upgrade -y -r ppp --version vvv --source "'C:\Users\Thomas\Downloads;https://chocolatey.org/api/v2/'" --force
+choco install -y -r ppp --version vvv --source "'C:\Users\Test\Downloads;https://chocolatey.org/api/v2/'" --force
+choco upgrade -y -r ppp --version vvv --source "'C:\Users\Test\Downloads;https://chocolatey.org/api/v2/'" --force
 choco uninstall -y -r ppp
