daaf7a644b8efa2ef664972b126a653ebbc7e8cd
Thomas Démoulins <tdemoulins@gmail.com>
2023-11-05 08:56:34 +0100

    Remove x86 (32bits) version from VSCodium packages (#73)

--- vscodium.install/update.ps1
+++ vscodium.install/update.ps1
@@ -7,7 +7,6 @@ function global:au_GetLatest {
     Get-GitHubLatest `
         -Repository 'VSCodium/vscodium' `
         -FileType 'exe' `
-        -IsUri32 { param($Uri) $Uri -match '\bVSCodiumSetup-ia32\b' } `
         -IsUri64 { param($Uri) $Uri -match '\bVSCodiumSetup-x64\b' } `
         -Latest @{
             SoftwareName   = 'VSCodium'
@@ -23,11 +22,8 @@ function global:au_SearchReplace {
             "^([$]version\s*=\s*)'.*'$"           = "`$1'$($Latest.Version)'"
             "^(\s*packageName\s*=\s*)'.*'$"       = "`$1'$($Latest.PackageName)'"
             "^(\s*fileType\s*=\s*)'.*'$"          = "`$1'$($Latest.FileType)'"
-            "^(\s*url\s*=\s*)'.*'$"               = "`$1'$($Latest.Url32)'"
             "^(\s*url64bit\s*=\s*)'.*'$"          = "`$1'$($Latest.Url64)'"
             "^(\s*silentArgs\s*=\s*)'.*'$"        = "`$1'$($Latest.SilentArgs)'"
-            "^(\s*checksum\s*=\s*)'.*'$"          = "`$1'$($Latest.Checksum32)'"
-            "^(\s*checksumType\s*=\s*)'.*'$"      = "`$1'$($Latest.ChecksumType32)'"
             "^(\s*checksum64\s*=\s*)'.*'$"        = "`$1'$($Latest.Checksum64)'"
             "^(\s*checksumType64\s*=\s*)'.*'$"    = "`$1'$($Latest.ChecksumType64)'"
             "^(\s*validExitCodes\s*=\s*)@\(.*\)$" = "`$1@($($Latest.ValidExitCodes))"
@@ -36,5 +32,5 @@ function global:au_SearchReplace {
 }
 
 if ($MyInvocation.InvocationName -ne '.') {
-    Update-Package -ChecksumFor all -Force:$Force
+    Update-Package -ChecksumFor 64 -Force:$Force
 }
