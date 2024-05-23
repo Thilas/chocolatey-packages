5716374c49d702feda3b3b4e77d72c12c6f6d3bc
Thomas Démoulins <tdemoulins@gmail.com>
2024-05-23 11:36:50 +0200

    Switch AU source

--- .vscode/debug.ps1
+++ .vscode/debug.ps1
@@ -3,7 +3,7 @@ param(
     [Parameter(Mandatory)] [string] $Path
 )
 
-$module = "$PSScriptRoot\..\..\majkinetor\au\AU"
+$module = "$PSScriptRoot\..\..\chocolatey\chocolatey-au\AU"
 if (Test-Path $module) {
     Get-Module AU | Remove-Module
     Import-Module $module
