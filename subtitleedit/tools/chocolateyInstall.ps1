a67d88535bbb1f36136c4a6844fb8757f3147b3f
Thomas Démoulins <tdemoulins@gmail.com>
2026-03-08 16:48:03 +0100

    Fix subtitleedit

--- subtitleedit/tools/chocolateyInstall.ps1
+++ subtitleedit/tools/chocolateyInstall.ps1
@@ -1,46 +1,23 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
-
-$packageCacheLocation = Get-PackageCacheLocation
 . $toolsDir\helpers.ps1
-
-# *** Automatically filled ***
-$packageArgs = @{
-    packageName   = 'subtitleedit'
-    url           = 'https://github.com/SubtitleEdit/subtitleedit/releases/download/4.0.15/SubtitleEdit-4.0.15-Setup.zip'
-    unzipLocation = $packageCacheLocation
-    checksum      = 'af9c17279b46b5e20624b99f2eec1fe376c8bc5abeeaf553fadbec588ebff22f'
-    checksumType  = 'sha256'
-}
-# *** Automatically filled ***
-
-Install-ChocolateyZipPackage @packageArgs
 
 # *** Automatically filled ***
 $packageArgs = @{
     packageName    = 'subtitleedit'
     softwareName   = 'Subtitle Edit*'
     fileType       = 'exe'
+    url            = 'https://github.com/SubtitleEdit/subtitleedit/releases/download/4.0.15/SubtitleEdit-4.0.15-Setup.zip'
     silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+    checksum       = 'af9c17279b46b5e20624b99f2eec1fe376c8bc5abeeaf553fadbec588ebff22f'
+    checksumType   = 'sha256'
     validExitCodes = @(0)
 }
 # *** Automatically filled ***
-
-$file = "$packageCacheLocation\*.{0}" -f $packageArgs.fileType | Get-Item | ForEach-Object FullName
-if (!$file) {
-    throw "Invalid zip file: installer is missing."
-}
-if ($file -is [array]) {
-    throw "Invalid zip file: multiple installers found."
-}
 
 $pp = Get-PackageParameters
 $mergeTasks = Get-MergeTasks $pp
 $packageArgs.silentArgs += ' /MERGETASKS="{0}"' -f $mergeTasks
 
-Install-ChocolateyInstallPackage @packageArgs -File $file
-
-$logPath = Join-Path $toolsDir '*.zip.txt'
-Remove-Item $logPath -Force -ErrorAction SilentlyContinue
-Remove-Item $packageCacheLocation -Recurse -Force -ErrorAction SilentlyContinue
+Install-ChocolateyPackage @packageArgs
