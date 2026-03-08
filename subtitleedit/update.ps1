17d0e64694df34e544e925336c70ce18b1cfa9ba
Thomas Démoulins <tdemoulins@gmail.com>
2026-03-08 16:51:59 +0100

    Fix subtitleedit (#107)

--- subtitleedit/update.ps1
+++ subtitleedit/update.ps1
@@ -6,16 +6,12 @@ param([switch] $Force)
 function global:au_GetLatest {
     Get-GitHubLatest `
         -Repository 'SubtitleEdit/subtitleedit' `
-        -FileType 'zip' `
-        -IsUri32 { param($Uri) $Uri -match '\bSetup\b' } `
+        -FileType 'exe' `
+        -IsUri64 { param($Uri) $Uri -match '\bx64\b' } `
         -Latest @{
-            SoftwareName            = 'Subtitle Edit*'
-            FileType                = 'exe'
-            SilentArgs              = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-            ValidExitCodes          = '0'
-            UninstallFileType       = 'exe'
-            UninstallSilentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-            UninstallValidExitCodes = '0'
+            SoftwareName   = 'Subtitle Edit*'
+            SilentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+            ValidExitCodes = '0'
         }
 }
 
@@ -25,20 +21,13 @@ function global:au_SearchReplace {
             "^(\s*packageName\s*=\s*)'.*'$"       = "`$1'$($Latest.PackageName)'"
             "^(\s*softwareName\s*=\s*)'.*'$"      = "`$1'$($Latest.SoftwareName)'"
             "^(\s*fileType\s*=\s*)'.*'$"          = "`$1'$($Latest.FileType)'"
-            "^(\s*url\s*=\s*)'.*'$"               = "`$1'$($Latest.Url32)'"
+            "^(\s*url64bit\s*=\s*)'.*'$"          = "`$1'$($Latest.Url64)'"
             "^(\s*silentArgs\s*=\s*)'.*'$"        = "`$1'$($Latest.SilentArgs)'"
-            "^(\s*checksum\s*=\s*)'.*'$"          = "`$1'$($Latest.Checksum32)'"
-            "^(\s*checksumType\s*=\s*)'.*'$"      = "`$1'$($Latest.ChecksumType32)'"
+            "^(\s*checksum64\s*=\s*)'.*'$"        = "`$1'$($Latest.Checksum64)'"
+            "^(\s*checksumType64\s*=\s*)'.*'$"    = "`$1'$($Latest.ChecksumType64)'"
             "^(\s*validExitCodes\s*=\s*)@\(.*\)$" = "`$1@($($Latest.ValidExitCodes))"
-        }
-        'tools\chocolateyUninstall.ps1' = @{
-            "^([$]packageName\s*=\s*)'.*'$"       = "`$1'$($Latest.PackageName)'"
-            "^([$]softwareName\s*=\s*)'.*'$"      = "`$1'$($Latest.SoftwareName)'"
-            "^([$]fileType\s*=\s*)'.*'$"          = "`$1'$($Latest.UninstallFileType)'"
-            "^([$]silentArgs\s*=\s*)'.*'$"        = "`$1'$($Latest.UninstallSilentArgs)'"
-            "^([$]validExitCodes\s*=\s*)@\(.*\)$" = "`$1@($($Latest.UninstallValidExitCodes))"
         }
     }
 }
 
-Update-Package -ChecksumFor 32 -Force:$Force
+Update-Package -ChecksumFor 64 -Force:$Force
