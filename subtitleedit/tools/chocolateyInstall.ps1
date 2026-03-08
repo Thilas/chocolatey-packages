48d1804508a5463d1541a4dd2a205a89a945e1ad
Thomas Démoulins <tdemoulins@gmail.com>
2026-03-08 16:50:09 +0100

    fix

--- subtitleedit/tools/chocolateyInstall.ps1
+++ subtitleedit/tools/chocolateyInstall.ps1
@@ -8,10 +8,10 @@ $packageArgs = @{
     packageName    = 'subtitleedit'
     softwareName   = 'Subtitle Edit*'
     fileType       = 'exe'
-    url            = 'https://github.com/SubtitleEdit/subtitleedit/releases/download/4.0.15/SubtitleEdit-4.0.15-Setup.zip'
+    url64bit       = ''
     silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-    checksum       = 'af9c17279b46b5e20624b99f2eec1fe376c8bc5abeeaf553fadbec588ebff22f'
-    checksumType   = 'sha256'
+    checksum64     = ''
+    checksumType64 = 'sha256'
     validExitCodes = @(0)
 }
 # *** Automatically filled ***
