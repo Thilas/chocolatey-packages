903817d31c9e57afbb973d645ad809029e2cdb00
Thomas Démoulins <tdemoulins@gmail.com>
2026-05-30 13:17:46 +0200

    Add new package for plezy

---
+++ plezy/tools/chocolateyUninstall.ps1
@@ -0,0 +1,6 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$shortcutPath = Join-Path $([Environment]::GetFolderPath([System.Environment+SpecialFolder]::CommonPrograms)) 'Plezy.lnk'
+if (Test-Path $shortcutPath) {
+    Remove-Item $shortcutPath
+}
---
+++ plezy/update.ps1
@@ -0,0 +1,28 @@
+﻿[CmdletBinding()]
+param([switch] $Force)
+
+. "$PSScriptRoot\..\Common.ps1"
+
+function global:au_GetLatest {
+    Get-GitHubLatest `
+        -Repository 'edde746/plezy' `
+        -FileType '7z' `
+        -IsUri64 { param($Uri) $Uri -match '\bwindows\b.+\bx64\b' }
+}
+
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "^(\s*packageName\s*=\s*)'.*'$" = "`$1'$($Latest.PackageName)'"
+            "^([$]fileType\s*=\s*)'.*'$"    = "`$1'$($Latest.FileType)'"
+        }
+        'legal\VERIFICATION.txt' = @{
+            "(?i)(\s+x64:).*"               = "`$1 $($Latest.Url64)"
+            "(?i)(checksum64:).*"           = "`$1 $($Latest.Checksum64)"
+        }
+    }
+}
+
+Update-Package -ChecksumFor none -Force:$Force
