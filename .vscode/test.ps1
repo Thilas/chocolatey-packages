19777a58af86b9be4ef8b1ed3f64997e885a8a48
Thomas Démoulins <tdemoulins@gmail.com>
2026-06-07 14:57:21 +0200

    Update local test script

--- .vscode/test.ps1
+++ .vscode/test.ps1
@@ -6,7 +6,11 @@ param(
 $ErrorActionPreference = 'Stop'
 
 # Check if Windows Sandbox is enabled
-if (!(Get-Command WindowsSandbox -ErrorAction SilentlyContinue)) {
+$windowsSandboxCommand = Get-Command WindowsSandbox -ErrorAction SilentlyContinue
+$wsbCommand = Get-Command wsb -ErrorAction SilentlyContinue
+if (!$windowsSandboxCommand -or !$wsbCommand) {
+    if (!$windowsSandboxCommand) { Write-Warning 'WindowsSandbox command is missing.' }
+    if (!$wsbCommand) { Write-Warning 'wsb command is missing.' }
     Write-Error @'
 Windows Sandbox does not seem to be available. Check the following URL for prerequisites and further details:
 https://docs.microsoft.com/en-us/windows/security/threat-protection/windows-sandbox/windows-sandbox-overview
@@ -18,13 +22,10 @@ $ Enable-WindowsOptionalFeature -Online -FeatureName 'Containers-DisposableClien
 
 # Close Windows Sandbox
 
-$sandbox = Get-Process WindowsSandboxClient -ErrorAction SilentlyContinue
-if ($sandbox) {
-    '--> Closing Windows Sandbox'
-    $sandbox | Stop-Process
-    Start-Sleep -Seconds 5
+wsb list --raw | ConvertFrom-Json | ForEach-Object WindowsSandboxEnvironments | ForEach-Object {
+    '--> Closing Windows Sandbox {0}' -f $_.Id
+    wsb stop --id $_.Id
 }
-Remove-Variable sandbox
 
 # Initialize Temp Folder
 
@@ -41,53 +42,49 @@ $packageName = if (Test-Path *.nuspec) {
 # Create Bootstrap script
 $bootstrapPs1FileName = 'Bootstrap.ps1'
 $bootstraPath = "$tempFolder\$bootstrapPs1FileName"
-@"
-cd ~\Desktop
-'--> Installing Chocolatey'
-`$sw = [System.Diagnostics.Stopwatch]::StartNew()
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-'--> Chocolatey installed in {0}' -f `$sw.Elapsed
-'--> Enabling automatic confirmation for Chocolatey'
-choco feature enable -n=allowGlobalConfirmation
-"@ > $bootstraPath
-if ($packageName) {
-    $chocoInstallCommand = "choco install $packageName --version $version --source `"$packageName;chocolatey`" $ChocoParameters"
-    @"
-'--> Installing $packageName.$version'
-'$chocoInstallCommand'
-`$sw = [System.Diagnostics.Stopwatch]::StartNew()
-$chocoInstallCommand
-'--> Package installed in {0}' -f `$sw.Elapsed
-"@ >> $bootstraPath
-}
+Set-Content $bootstraPath -Value @(
+    'cd ~\Desktop'
+    '"--> Installing Chocolatey"'
+    '$sw = [System.Diagnostics.Stopwatch]::StartNew()'
+    "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
+    '"--> Chocolatey installed in {0}" -f $sw.Elapsed'
+    '"--> Enabling automatic confirmation for Chocolatey"'
+    'choco feature enable -n=allowGlobalConfirmation'
+    if ($packageName) {
+        $chocoInstallCommand = "choco install $packageName --version $version --source `"$packageName;chocolatey`" $ChocoParameters"
+        "'--> Installing $packageName.$version'"
+        "'$chocoInstallCommand'"
+        '$sw = [System.Diagnostics.Stopwatch]::StartNew()'
+        $chocoInstallCommand
+        '"--> Package installed in {0}" -f $sw.Elapsed'
+    }
+)
 
 # Create wsb file
 $sandboxTestWsbFile = "$tempFolder\SandboxTest.wsb"
-@"
-<Configuration>
-  <MappedFolders>
-    <MappedFolder>
-      <HostFolder>$tempFolder</HostFolder>
-      <ReadOnly>true</ReadOnly>
-    </MappedFolder>
-    <MappedFolder>
-      <HostFolder>$PWD</HostFolder>
-    </MappedFolder>
-  </MappedFolders>
-  <LogonCommand>
-    <Command>PowerShell Start-Process PowerShell -WindowStyle Maximized -WorkingDirectory '~\Desktop\$tempName' -ArgumentList '-ExecutionPolicy Bypass -NoExit -NoLogo -File $bootstrapPs1FileName'</Command>
-  </LogonCommand>
-</Configuration>
-"@ > $sandboxTestWsbFile
+Set-Content $sandboxTestWsbFile -Value @(
+    '<Configuration>'
+    '  <MappedFolders>'
+    '    <MappedFolder>'
+    "      <HostFolder>$tempFolder</HostFolder>"
+    '      <ReadOnly>true</ReadOnly>'
+    '    </MappedFolder>'
+    '    <MappedFolder>'
+    "      <HostFolder>$PWD</HostFolder>"
+    '    </MappedFolder>'
+    '  </MappedFolders>'
+    '  <LogonCommand>'
+    "    <Command>PowerShell Start-Process PowerShell -WindowStyle Maximized -WorkingDirectory '~\Desktop\$tempName' -ArgumentList '-ExecutionPolicy Bypass -NoExit -NoLogo -File $bootstrapPs1FileName'</Command>"
+    '  </LogonCommand>'
+    '</Configuration>'
+)
 
-@"
---> Starting Windows Sandbox, and:
-  - Mounting the following directories:
-    - $tempFolder
-    - $PWD
-  - Installing Chocolatey
-"@
+'--> Starting Windows Sandbox, and:'
+'  - Mounting the following directories:'
+"    - $tempFolder"
+"    - $PWD"
+'  - Installing Chocolatey'
 if ($packageName) {
     "  - Installing $packageName.$version"
 }
-WindowsSandbox $SandboxTestWsbFile
+WindowsSandbox $sandboxTestWsbFile
